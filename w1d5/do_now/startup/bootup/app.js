7223fe944dd6ebb967aad5f1ff4243a71cb2dec0
Isabel Gormley <isabelgormley@Isabels-MacBook.local>
2017-07-14 14:13:12 -0400

    pushing html

--- w1d5/do_now/startup/bootup/app.js
+++ w1d5/do_now/startup/bootup/app.js
@@ -1,1 +1,53 @@
-console.log("buddha");
+/*function final (x,y,z){
+	const answer = (x * y * z);
+	return answer;
+}
+const multiply = final(3,4,5);
+alert(multiply);*/
+
+/*function fortune (a,b,x,y){
+	const result ="You will be a "+a+" in "+b+", and married to "+x+" with "+y+" kids.";
+	return result;
+}
+const answer = fortune("professional mourner", "Tallahassee", "Riley", 17);
+alert(answer);*/
+
+/*function age(x){
+	const years = (x * 7);
+	const result = "Your doggie is "+years+" years old in dog years.";
+	return result;
+}
+const final = age(5);
+alert(final);*/
+
+/*function lifetime(age1,age2,amount){
+	const answer = (age2-age1) * amount * 365;
+	const supply = "You will need "+answer+" cups of water to last until the ripe old age of "+age2+"."
+	return supply;
+}
+const final = lifetime(15,189,8)
+alert(final);*/
+
+/*function calcCircumference(radius){
+	const circum = radius * 2 * Math.PI;
+	const final = "The circumference is "+circum+".";
+	return circum;
+}
+const answer= calcCircumference(5);
+alert(answer);*/
+
+/*function calcArea(radius){
+	const area = (radius*radius) * Math.PI;
+	const final2 = "The area is "+area+".";
+	return final2;
+}
+const answer2= calcArea(5);
+alert(answer2);*/
+
+/*function cToF(celcius){
+	const answer = celcius * (9/5) + 32;
+	const final = "If it is " + celcius+ " degrees celcius, it is " +answer+" degrees fahrenheit.";
+	return final;
+}
+const temp = cToF(32);
+alert(temp);*/
